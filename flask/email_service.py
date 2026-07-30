742a1691161f06d492b388a87690e303ff7e7192
emmanuel <emmanuel.abbah@pau.edu.ng>
2026-07-30 13:34:04 +0100

    Added all project dependencies

--- flask/email_service.py
+++ flask/email_service.py
@@ -1,22 +1,50 @@
+
 from brevo import Brevo
-from config import Config
 from flask import current_app
-from brevo.transactional_emails import (SendTransacEmailRequestSender, SendTransacEmailRequestToItem)
+from brevo.transactional_emails import (
+    SendTransacEmailRequestSender,
+    SendTransacEmailRequestToItem
+)
+
 
 def send_verification_email(email, subject, html):
     client = Brevo(
         api_key=current_app.config["BREVO_API_KEY"]
     )
+
     client.transactional_emails.send_transac_email(
-    subject=subject,
-    html_content=html,
-    sender=SendTransacEmailRequestSender(
-        name=current_app.config("MAIL_FROM_TITLE"),
-        email=current_app.config("MAIL_FROM")
-    ),
-    to={
-        SendTransacEmailRequestToItem(
-            email=email
-        )
-    }
+        subject=subject,
+        html_content=html,
+        sender=SendTransacEmailRequestSender(
+            name=current_app.config["MAIL_FROM_TITLE"],
+            email=current_app.config["MAIL_FROM"]
+        ),
+        to=[
+            SendTransacEmailRequestToItem(
+                email=email
+            )
+        ]
     )
+
+# from brevo import Brevo
+# from config import Config
+# from flask import current_app
+# from brevo.transactional_emails import (SendTransacEmailRequestSender, SendTransacEmailRequestToItem)
+
+# def send_verification_email(email, subject, html):
+#     client = Brevo(
+#         api_key=current_app.config["BREVO_API_KEY"]
+#     )
+#     client.transactional_emails.send_transac_email(
+#     subject=subject,
+#     html_content=html,
+#     sender=SendTransacEmailRequestSender(
+#         name=current_app.config("MAIL_FROM_TITLE"),
+#         email=current_app.config("MAIL_FROM")
+#     ),
+#     to={
+#         SendTransacEmailRequestToItem(
+#             email=email
+#         )
+#     }
+#     )
