58764393090580cbaac840490c0e3da4bf57d414
emmanuel <emmanuel.abbah@pau.edu.ng>
2026-08-03 12:38:57 +0100

    correct: initialized the JWT Manager

--- flask/extension.py
+++ flask/extension.py
@@ -1,9 +1,11 @@
 # from flask_mysqldb import MYSQL #alt for pymysql
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
+from flask_jwt_extended import JWTManager
 
 
 bcrypt = Bcrypt()
 mail = Mail()
+jwt = JWTManager()
 
 #THis file is used to create package needed to be initialized
