4d24dbd320a6ec733fdeb5cd1c0852b4d489d688
emmanuel <emmanuel.abbah@pau.edu.ng>
2026-08-04 23:19:36 +0100

    added a form to make the app a mini fullstack application

---
+++ flask/mha-app/src/App.tsx
@@ -0,0 +1,18 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+
+import Signup from './components/Signup';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ flask/mha-app/src/components/Signup.tsx
@@ -0,0 +1,40 @@
+export default function Signup() {
+  return (
+    <div
+      className="h-screen w-screen bg-cover bg-center flex items-center justify-center px-4 sm:px-6 lg:px-8"
+      style={{ backgroundImage: "url('/Background.jpg')" }}
+    >
+      <div className="w-full max-w-md bg-white/10 backdrop-blur-md rounded-2xl shadow-2xl p-8">
+        <h2 className="text-white text-3xl sm:text-4xl font-bold mb-10 text-center drop-shadow-lg">
+          SIGN UP
+        </h2>
+        <p className="text-white text-center ">Create an account here to start</p>
+        <form className="flex flex-col gap-4">
+          <input
+            type="email"
+            placeholder="email address"
+            className="p-3 rounded-xl border  border-white bg-white/20 mt-6 placeholder-white text-white focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="p-3 rounded-xl border border-white bg-white/20 placeholder-white text-white focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-3 rounded-xl font-semibold hover:bg-blue-600 transition"
+          >
+            SIGN UP
+          </button>
+        </form>
+
+        <p className="text-sm mt-4 text-center text-white drop-shadow-md">
+          Already have an account? {""}
+          <a href="/login" className="underline">
+            Log In
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
