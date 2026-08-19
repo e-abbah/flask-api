e71c82d04ded1f4e59f660590ce0d5e58507180a
emmanuel <emmanuel.abbah@pau.edu.ng>
2026-08-19 01:08:44 +0100

    feat: add home route to courses blueprint

--- flask/courses/courses.py
+++ flask/courses/courses.py
@@ -2,6 +2,11 @@ from flask import Blueprint
 
 courses_bp = Blueprint("courses", __name__)
 
+@courses_bp.route("/", methods=["GET"])
+def home():
+    return "<p>Welcome to Flask</p>"
+
+
 @courses_bp.route("/courses", methods=["GET"])
 def get_courses():
     return "<p>Welcome to course endpoint</p>"
